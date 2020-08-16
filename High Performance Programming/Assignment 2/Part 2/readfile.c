0b7fd44ac7b6938e09ddc403e80d0fb04a7327a9
fredg96 <36188113+fredg96@users.noreply.github.com>
2020-08-16 15:50:26 +0200

    Add files via upload

--- High Performance Programming/Assignment 2/Part 2/readfile.c
+++ High Performance Programming/Assignment 2/Part 2/readfile.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    FILE *fp = fopen("little_bin_file", "r"); // open file for reading
+    int i; 
+	double dou; 
+	char ch; 
+	float floa; 
+	fread(&i, sizeof(int), 1, fp);
+	fread(&dou, sizeof(double), 1, fp);
+	fread(&ch, sizeof(char), 1, fp);
+	fread(&floa, sizeof(float), 1, fp);
+	fclose(fp);
+	printf("%d\n%f\n%c\n%f\n", i, dou, ch, floa);
+    return 0;
+}
 

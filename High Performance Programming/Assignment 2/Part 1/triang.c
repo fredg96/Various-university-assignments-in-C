bc8a79d812eea576646a9082752bc38eca775e22
fredg96 <36188113+fredg96@users.noreply.github.com>
2020-08-16 15:49:25 +0200

    Add files via upload

--- High Performance Programming/Assignment 2/Part 1/triang.c
+++ High Performance Programming/Assignment 2/Part 1/triang.c
@@ -1,1 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+
+
+// The main program.
+int main( int argc, char *argv[] )
+{
+    if(argc != 2) {
+        printf("Incorrect number of input arguemnts. \n"); 
+        return 1; 
+    } 
+    int num = atof(argv[1]);
+    int i, j, val;
+    for(i = 0; i < num; i++) {
+        for(j = 0; j <= i; j++) {
+            if(j == 0) {
+                val = 1; 
+                printf("%d\t", val);
+            } else{
+                val = val * (i+1-j)/j;
+                printf("%d\t", val);
+            }
+        }
+        printf("\n");
+    }
+
+    return 0; 
+}
